df76ab3622a50e1e7074a6b90f584c598673390d
Kirk Lestelle <kirklest@uw.edu>
2015-10-26 14:23:59 -0700

    Finished move-after task

--- js/main.js
+++ js/main.js
@@ -2,3 +2,37 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+// changes the content of the specified element
+$('#change-content').on('click', function() {
+    var selector = $('#selector').val();
+    var content = $('#newContent').val();
+    $(selector).text(content);
+});
+
+// adds the content at the end of the element
+$('#add-at-end').on('click', function() {
+    var selector = $('#selector').val();
+    var content = $('#newContent').val();
+    $(selector).append(content);
+});
+
+// add the content at the beginning of the element
+$('#add-at-start').on('click', function() {
+    var selector = $('#selector').val();
+    var content = $('#newContent').val();
+    $(selector).prepend(content);
+});
+
+// adds the content before the element
+$('#insert-before').on('click', function() {
+    var selector = $('#selector').val();
+    var content = $('#newContent').val();
+    $(selector).before($(content));
+});
+
+// moves the "element" to be after the element selected by the "content" (we're re-using the inputs, even if the labels don't match)
+$('#move-after').on('click', function() {
+    var selector = $('#selector').val();
+    var content = $('#newContent').val();
+    $(selector).insertAfter(content);
+});
